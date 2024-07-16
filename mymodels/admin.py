acf38daf03730b76754b953e2f4c1fc6bab2afd8
iskander <yangurazov2002@gmail.com>
2024-07-16 16:12:33 +0500

    Зарегистрировал модели в админке

--- mymodels/admin.py
+++ mymodels/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+import models
 
-# Register your models here.
+
+@admin.register(models.Person)
+class Person(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_display_links = ('first_name',)
+
+
+@admin.register(models.Address)
+class Address(admin.ModelAdmin):
+    list_display = ('person', 'city')
+    list_display_links = ('person', 'city')
+
+
+@admin.register(models.Project)
+class Project(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_display_links = ('name',)
+
+
+@admin.register(models.Document)
+class Document(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('title',)
